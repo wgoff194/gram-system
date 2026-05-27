5414cb10dbb3fb29c749f6b972bccf1c41350428
Warren G Brown <warren.brown@e-hps.com>
2026-05-27 10:41:06 -0400

    updated files to GRAM

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,18 +1,1 @@
-## 1.2.0
 
-- Added "Resource" property to game tokens, which includes a max & current value for a bar.
-- Removed assumptions about AE behavior in anticipation of v14 updates.
-
-## 1.1.2
-
-- Added "Count" property to game tokens
-
-## 1.1.1
-
-- Fixed display of actor image for non-square images.
-
-## 1.1.0
-
-- Added "Game Token" as a generic default actor type.
-- Active Effects with expired durations are now suppressed.
-- Minor styling fixes.
--- src/module/config.mjs
+++ src/module/config.mjs
@@ -1,38 +1,3 @@
 const GRAM = {};
 
-GRAM.chess = {
-  pieces: {
-    pawn: {
-      label: "GRAM.Chess.Pieces.P",
-      abbr: "P",
-      value: 1,
-    },
-    knight: {
-      label: "GRAM.Chess.Pieces.K",
-      abbr: "N",
-      value: 3,
-    },
-    bishop: {
-      label: "GRAM.Chess.Pieces.B",
-      abbr: "B",
-      value: 3,
-    },
-    rook: {
-      label: "GRAM.Chess.Pieces.R",
-      abbr: "R",
-      value: 5,
-    },
-    queen: {
-      label: "GRAM.Chess.Pieces.Q",
-      abbr: "Q",
-      value: 9,
-    },
-    king: {
-      label: "GRAM.Chess.Pieces.K",
-      abbr: "K",
-      value: Infinity,
-    },
-  },
-};
-
 export default GRAM;
